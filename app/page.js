6eef5e5cc2b87e6400ff3fb38806636ff61a4b7c
Julián Millar <julianjmillar@gmail.com>
2023-09-21 16:06:09 -0300

    added form and styles

--- app/page.js
+++ app/page.js
@@ -3,10 +3,10 @@ import classes from '@/app/styles.module.scss'
 export default function Home () {
   return (
     <section className={classes.card}>
-      <form>
+      <form className={classes.form}>
         <input className={classes.input} type='text' id='user' name='user' placeholder='nombre de usuario' />
         <input className={classes.input} type='password' id='password' name='password' placeholder='contraseña' />
-        <button className={classes.button} type='button'>ENTRAR</button>
+        <button className={classes.button} type='submit'>ENTRAR</button>
       </form>
     </section>
   )
